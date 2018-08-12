ad8defa1d5d790e166a865e75f9a7dbca2d15137
Jonathan <jalmeida0291@gmail.com>
2018-08-12 09:03:44 -0400

    improved ui and refactored js code

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
-const button = document.querySelector('.btn');
-const quote = document.querySelector('.quote')
-const author = document.querySelector('.author');
-const body = document.querySelector('body');
+const button = document.querySelector(".btn");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const title = document.querySelector(".title");
+const container = document.querySelector(".container");
 
 let color = {
-    red: null,
-    green: null,
-    blue: null,
-    opacity: null
+  red: null,
+  green: null,
+  blue: null
+};
+
+ajaxRequest();
+
+button.addEventListener("click", ajaxRequest);
+
+function ajaxRequest() {
+  let ajax = new XMLHttpRequest();
+  ajax.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let parsedArr = JSON.parse(this.responseText);
+      let randomNum = Math.round(Math.random() * parsedArr.length - 1);
+      quote.textContent = parsedArr[randomNum].text;
+      author.textContent = parsedArr[randomNum].from;
+      setColors();
+    }
+  };
+  ajax.open("get", "quotes.json", true);
+  ajax.send();
 }
 
-
-body.style.backgroundColor = generateRandomColor(color);
-
-button.addEventListener('click', function () {
-    let ajax = new XMLHttpRequest();
-    ajax.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let parsedArr = JSON.parse(this.responseText);
-            let randomNum = Math.round(Math.random() * parsedArr.length - 1);
-            // let quote = parsedArr[randomNum];
-            // document.querySelector('p').textContent = JSON.stringify(quote);
-            console.log(parsedArr[randomNum])
-            quote.textContent = parsedArr[randomNum].quote;
-            author.textContent = parsedArr[randomNum].name;
-            body.style.backgroundColor = generateRandomColor(color);
-        }
-    }
-    ajax.open('get', 'quotes.json', true);
-    ajax.send();
-});
-
 function generateRandomColor(colorObj) {
-    colorObj.red = Math.round(Math.random() * 255);
-    colorObj.green = Math.round(Math.random() * 255);
-    colorObj.blue = Math.round(Math.random() * 255);
-    colorObj.opacity = Math.round(Math.random() * 10) * .1;
-    return `rgba(${colorObj.red},${colorObj.green},${colorObj.blue},${colorObj.opacity})`;
+  colorObj.red = Math.round(Math.random() * 255);
+  colorObj.green = Math.round(Math.random() * 255);
+  colorObj.blue = Math.round(Math.random() * 255);
+  return `rgb(${colorObj.red},${colorObj.green},${colorObj.blue})`;
+}
+
+function setColors() {
+  let randColor = generateRandomColor(color);
+  quote.style.color = randColor;
+  author.style.color = randColor;
+  title.style.color = randColor;
+  button.style.backgroundColor = randColor;
+  container.style.borderColor = randColor;
 }
