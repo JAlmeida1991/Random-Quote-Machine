c618b5803f0ee21202a15560eb0d439f856ef7e9
Jonathan <jalmeida0291@gmail.com>
2018-09-29 17:11:40 -0400

    fixed randomNum bug and refactored js

--- app.js
+++ app.js
@@ -1,80 +1,78 @@
-// Global variables:
-const button = document.querySelector(".btn");
-const quote = document.querySelector(".quote");
-const author = document.querySelector(".author");
-const title = document.querySelector(".title");
-const container = document.querySelector(".container");
+(function() {
+  "use strict";
+  // Global variables:
+  const button = document.querySelector(".btn");
+  const quote = document.querySelector(".quote");
+  const author = document.querySelector(".author");
+  const title = document.querySelector(".title");
+  const container = document.querySelector(".container");
 
-const color = {
-  red: null,
-  green: null,
-  blue: null
-};
+  const color = {
+    red: null,
+    green: null,
+    blue: null
+  };
 
-// Event handler:
+  // Event handler:
 
-button.addEventListener("click", fetchAsyncAwait);
-window.addEventListener("load", fetchAsyncAwait);
+  button.addEventListener("click", fetchAsyncAwait);
 
-// Helper functions
+  window.addEventListener("load", fetchAsyncAwait);
 
-// Version 3 using Async Await:
-async function fetchAsyncAwait() {
-  try {
-    const req = await fetch("./data/quotes.json");
-    const res = await req.json();
-    const randomNum = Math.round(Math.random() * res.length - 1);
-    quote.textContent = res[randomNum].text;
-    author.textContent = res[randomNum].from;
-    setColors();
-    // If for some reason fetch is unsuccessful, execute the following functions
-  } catch (err) {
-    const error = err.name;
-    quote.remove();
-    button.remove();
-    loadError(error);
+  // Helper functions
+
+  // Version 3 using Async Await:
+  async function fetchAsyncAwait() {
+    try {
+      const req = await fetch("./data/quotes.json");
+      const res = await req.json();
+      const randomNum = Math.round(Math.random() * (res.length - 1));
+      quote.textContent = res[randomNum].text;
+      author.textContent = res[randomNum].from;
+      setColors();
+      // If for some reason fetch is unsuccessful, execute the following functions
+    } catch (err) {
+      const error = err.name;
+      quote.remove();
+      button.remove();
+      title.remove();
+      loadError(error);
+    }
   }
-}
 
-function generateRandomColor({ red, green, blue }) {
-  red = Math.round(Math.random() * 255);
-  green = Math.round(Math.random() * 255);
-  blue = Math.round(Math.random() * 255);
-  return `rgb(${red},${green},${blue})`;
-}
+  function generateRandomColor({ red, green, blue }) {
+    red = Math.round(Math.random() * 255);
+    green = Math.round(Math.random() * 255);
+    blue = Math.round(Math.random() * 255);
+    return `rgb(${red},${green},${blue})`;
+  }
 
-function setColors() {
-  const randColor = generateRandomColor(color);
-  quote.style.color = randColor;
-  author.style.color = randColor;
-  title.style.color = randColor;
-  button.style.backgroundColor = randColor;
-  container.style.borderColor = randColor;
-}
+  function setColors() {
+    const randColor = generateRandomColor(color);
+    quote.style.color = randColor;
+    author.style.color = randColor;
+    title.style.color = randColor;
+    button.style.backgroundColor = randColor;
+    container.style.borderColor = randColor;
+  }
 
-function loadError(err) {
-  const errorColor = "#D32F2F";
-  const error = document.createElement("p");
+  function loadError(err) {
+    const errorColor = "#D32F2F";
+    const error = document.createElement("p");
 
-  // Remove border from container
-  container.style.border = "none";
+    // Remove border from container
+    container.style.border = "none";
 
-  // Set up styles for error
-  error.textContent = `Something went wrong... ${err}`;
-  error.style.color = errorColor;
-  error.style.textAlign = "center";
-  error.style.fontSize = "3rem";
-  // error.style.marginBottom = "1.8rem";
-  // error.style.fontWeight = 00;
-
-  // Set up styles for title
-  title.textContent = "ERROR...";
-  title.style.color = errorColor;
-  title.style.fontSize = "6rem";
-
-  // Append error to container
-  container.appendChild(error);
-}
+    // Set up styles for error
+    error.textContent = "Something went wrong...";
+    error.style.color = errorColor;
+    error.style.textAlign = "center";
+    error.style.fontSize = "3.5rem";
+    error.style.fontFamily = "fantasy";
+    // Append error to container
+    container.appendChild(error);
+  }
+})();
 
 //Version 1 using XMLHTttpRequest:
 
